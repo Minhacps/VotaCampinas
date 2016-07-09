01768d2db015785cc501d2165d28bf8487a08638
dsoldera <daniellesoldera@gmail.com>
2016-07-09 18:22:53 -0300

    corrido erro de login

--- src/modules/cadastro/cadastroController.js
+++ src/modules/cadastro/cadastroController.js
@@ -5,6 +5,7 @@
   var app = angular.module('votaCampinas');
 
   var cadastroController = function($scope) {    
+  	$('select').material_select();
   }
 
   app.controller('cadastroController', cadastroController);
