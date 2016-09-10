c35258190875f94e1525be38f4af898692d0b659
Guilherme Luchesi <gui.olhenrique@gmail.com>
2016-09-10 14:36:00 -0300

    feat(ranking): adiciona botoes de compartilhamento no face e no twitter

--- app/partials/ranking/rankingController.js
+++ app/partials/ranking/rankingController.js
@@ -9,6 +9,22 @@
     .then(function (res) {
       $scope.candidatos = res;
     });
+
+    (function(d, s, id) {
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) return;
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.7&appId=245108102541730";
+     fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
+     (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      }(document, "script", "twitter-wjs"));
   };
 
   app.controller('rankingController', rankingController);
